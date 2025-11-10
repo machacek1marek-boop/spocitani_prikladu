f75909de97d9eccc7bd746995da2e8bf750c5432
machacek1marek-boop <machacek1marek@gmail.com>
2025-11-10 16:09:52 +0100

    Add files via upload

---
+++ spocitaní prikladu.py
@@ -0,0 +1,23 @@
+print('cau')
+
+print('spočítam ti přiklad')
+cislo_1 = int(input('napiš číslo 1:'))
+znamenko = input('napiš znaménko:')
+cislo_2 = int(input('napiš číslo 2:'))
+
+#spocitaní
+if znamenko == '/':
+    vysledek = cislo_1/cislo_2    
+elif znamenko == '*':
+    vysledek = cislo_1*cislo_2    
+elif znamenko == '+':
+    vysledek = cislo_1+cislo_2   
+elif znamenko == '-':
+    vysledek = cislo_1-cislo_2
+else:
+    print('něco se pokazilo skontrolujte zadání')
+
+
+
+print(f'{cislo_1}{znamenko}{cislo_2} se rovna: {vysledek}')
+
